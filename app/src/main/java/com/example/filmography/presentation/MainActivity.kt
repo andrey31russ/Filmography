c49d1c75276016f07cb0a915fa0b2e8587e29c96
Andrei Boiarintsev <Boyarincev@t8.ru>
2023-11-17 15:07:31 +0300

    sd

--- app/src/main/java/com/example/filmography/presentation/MainActivity.kt
+++ app/src/main/java/com/example/filmography/presentation/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.filmography.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.filmography.R
+import com.example.filmography.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        //init()
     }
+
+//    private fun init(){
+//        //binding.buttomNavView.nav
+//    }
 }
--- app/src/main/java/com/example/filmography/presentation/screens/FragmentHome.kt
+++ app/src/main/java/com/example/filmography/presentation/screens/FragmentHome.kt
@@ -7,27 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.example.filmography.R
-import com.example.filmography.databinding.FragmentFilmDetaildBinding
+import com.example.filmography.databinding.FragmentHomeBinding
 
-
-class FilmDetaildFragment : Fragment() {
-    private lateinit var binding: FragmentFilmDetaildBinding
+class FragmentHome : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentFilmDetaildBinding.inflate(layoutInflater, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnBack.setOnClickListener {
-            it.findNavController().navigate(R.id.action_filmDetaildFragment_to_filmGenerateFragment)
-        }
+//        binding.button2.setOnClickListener {
+//            it.findNavController().navigate(R.id.action_fragmentHome_to_fragmentSearch)
+//        }
     }
-
-
 
 }
