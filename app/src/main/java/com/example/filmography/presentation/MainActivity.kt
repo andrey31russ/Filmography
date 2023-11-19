903d423d6a52a2f05d05d164bce2abb246e10346
abo <a50203@yandex.ru>
2023-11-19 14:17:53 +0300

    h

--- app/src/main/java/com/example/filmography/presentation/MainActivity.kt
+++ app/src/main/java/com/example/filmography/presentation/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.filmography.presentation.screens.FragmentHome
 import com.project.bindingapp.R
 
 
@@ -19,20 +18,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+        init()
 
     }
 
 
     private fun init(){
-        binding.bottomNavMenu.id = R.id.nav_settings
+
         binding.bottomNavMenu.setOnClickListener {
             when(it.id){
                 R.id.nav_home -> {Toast.makeText(this, "you have presed $it", Toast.LENGTH_SHORT).show()}
-                R.id.nav_search -> {}
-                R.id.nav_settings-> {}
+                R.id.nav_search -> {Toast.makeText(this, "you have presed $it", Toast.LENGTH_SHORT).show()}
+                R.id.nav_settings-> {Toast.makeText(this, "you have presed $it", Toast.LENGTH_SHORT).show()}
             }
 
         }
